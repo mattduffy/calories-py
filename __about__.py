9d5567dd9d781f38860e4be8ed373a54efc69070
Matthew Duffy <mattduffy@gmail.com>
2026-07-28 21:35:17 -0700

    Get rid of the __about__.py file.  I don't think it is part of how I am using hatch.

--- __about__.py
+++
@@ -1,9 +0,0 @@
-# __about__.py
-__title__ = "calories-py"
-__summary__ = "A dependency-free package that estimates calories burned during physical activities."
-__uri__ = "https://github.com/mattduffy/calories-py"
-__version__ = "0.0.1"
-__author__ = "Matthew Duffy"
-__email__ = "mattduffy@gmail.com"
-__license__ = "ISC"
-__copyright__ = f"2026 {__author__}"
