c6fb1e6e3f877aa35b2bdd83567f6ac397351438
Matthew Duffy <mattduffy@gmail.com>
2026-08-01 15:05:29 -0700

    Forgot to include the simpleCalories() function in the import and __all__ lines.

--- src/calories/__init__.py
+++ src/calories/__init__.py
@@ -1,6 +1,6 @@
 """Create a package for the calories.py module."""
 # load the public functions
-from .calories import pandolfCalories, lcdaCalories, minimumMechanicsCalories, calorieEnsemble, load_sample_data
+from .calories import simpleCalories, pandolfCalories, lcdaCalories, minimumMechanicsCalories, calorieEnsemble, load_sample_data
 
 # optional, but good practice
-__all__ = ["pandolfCalories", "lcdaCalories", "minimumMechanicsCalories", "calorieEnsemble", "load_sample_data"]
+__all__ = ["simpleCalories", "pandolfCalories", "lcdaCalories", "minimumMechanicsCalories", "calorieEnsemble", "load_sample_data"]
