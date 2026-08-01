2abcc0bf0050cd4b41e37dad31dda594cbcaf20e
Matthew Duffy <mattduffy@gmail.com>
2026-08-01 14:54:15 -0700

    Make changes to __init__.py to only export the *Calories(), calorieEnsemble() and load_sample_data() functions as a correctly formatted package.

--- src/calories/__init__.py
+++ src/calories/__init__.py
@@ -1,1 +1,6 @@
 """Create a package for the calories.py module."""
+# load the public functions
+from .calories import pandolfCalories, lcdaCalories, minimumMechanicsCalories, calorieEnsemble, load_sample_data
+
+# optional, but good practice
+__all__ = ["pandolfCalories", "lcdaCalories", "minimumMechanicsCalories", "calorieEnsemble", "load_sample_data"]
