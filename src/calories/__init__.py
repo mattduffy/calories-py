58559aca5c28809672d749016051f4bafd822754
Matthew Duffy <mattduffy@gmail.com>
2026-08-10 03:06:15 -0500

    Add __version__ to the list of package exports.

--- src/calories/__init__.py
+++ src/calories/__init__.py
@@ -2,5 +2,11 @@
 # load the public functions
 from .calories import simpleCalories, pandolfCalories, lcdaCalories, minimumMechanicsCalories, calorieEnsemble, load_sample_data
 
+try:
+    from ._version import __version__
+except ImportError:
+    # Fallback for local, unbuilt source trees
+    __version__ = "0.0.0.dev0"
+    
 # optional, but good practice
-__all__ = ["simpleCalories", "pandolfCalories", "lcdaCalories", "minimumMechanicsCalories", "calorieEnsemble", "load_sample_data"]
+__all__ = ["simpleCalories", "pandolfCalories", "lcdaCalories", "minimumMechanicsCalories", "calorieEnsemble", "load_sample_data", "__version__"]
